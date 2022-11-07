337a10a00b9ee6bd63acde0ebb651346012f457d
Ahsan Habib <ahsanpub16b@gmail.com>
2022-11-08 01:02:39 +0600

    added here booking slots not available at a time using react queary

--- index.js
+++ index.js
@@ -4,33 +4,58 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
- 
- 
+
+
 //use middleware
- 
+
 app.use(cors());
 app.use(express.json());
- 
- 
+
+
 // uri = pass and user name from .env file
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nfgmjj6.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // console.log(client);
- async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         // console.log("Database Connected");
         const servicesCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
 
-        app.get('/service',async(req,res) => {
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
+
+
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 11, 2022';
+            //step:1 => get all services 
+            const services = await servicesCollection.find().toArray();
+
+            //step:2 => get the booking of that day
+            const query = { date: date };
+            const booking = await bookingCollection.find(query).toArray();
+            // res.send(booking);
+
+            //step:3 => for each service , find bookings for that service
+            services.forEach(service => {
+                const serviceBooking = booking.filter(b => b.treatment === service.name);
+                const booked = serviceBooking.map(s => s.slot);
+                // service.booked = booked
+                // service.booked = serviceBooking.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+            })
+
+            res.send(services);
+        })
 
         /**
          * Api naming conversion :
@@ -47,31 +72,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
          * 
          */
 
-        app.post('/booking',async(req,res) =>{
+        app.post('/booking', async (req, res) => {
             const booking = req.body;
-            const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
             const exists = await bookingCollection.findOne(query);
             if (exists) {
-                return res.send({success: false, booking: exists})
+                return res.send({ success: false, booking: exists })
             }
             const result = await bookingCollection.insertOne(booking);
-            return res.send({success: true , result});
+            return res.send({ success: true, result });
         })
 
 
 
     }
-    finally{
+    finally {
 
     }
 
 }
 run().catch(console.dir);
- 
+
 app.get('/', (req, res) => {
     res.send('Running doctors Server');
 });
- 
-app.listen(port, () =>{
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
