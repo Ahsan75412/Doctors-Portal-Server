68d01bc6796652b39500eb3cc360a4b214a0fd68
Ahsan Habib <ahsanpub16b@gmail.com>
2022-11-07 23:56:03 +0600

    added here booking modal info section for booking an appointment

--- index.js
+++ index.js
@@ -23,13 +23,42 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
         await client.connect();
         // console.log("Database Connected");
         const servicesCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service',async(req,res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        /**
+         * Api naming conversion :
+         * 
+         * app.get('/booking') //get all booking in this collection.or get more one or by filter.
+         * app.get('/booking:id')//get a specific booking.
+         * app.post('/booking:id') // add a new booking.
+         * app.patch('/booking:id') // updating a booking.
+         * app.patch('/booking:id') // Deleting a booking.
+         * 
+         * Moral of the Story :
+         * when we write just 'booking' which means all booking collection here . 
+         * when we write 'booking:id' which means one booking here.
+         * 
+         */
+
+        app.post('/booking',async(req,res) =>{
+            const booking = req.body;
+            const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({success: false, booking: exists})
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({success: true , result});
         })
+
+
 
     }
     finally{
