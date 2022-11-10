a9812c3aca7c9f123e84d6437739a34f57a6b3ed
Ahsan Habib <ahsanpub16b@gmail.com>
2022-11-11 02:18:15 +0600

    sent email service with node mailer here

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
 
 
 //use middleware
@@ -42,6 +44,54 @@ function verifyJWT(req, res, next) {
 
 
 
+// email sender with sendGrid with hafsar email diye 
+
+
+const emailSenderOptions = {
+    auth: {
+      api_key: process.env.EMAIL_SENDER_KEY
+    }
+  }
+  
+  const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+  
+  function sendAppointmentEmail(booking){
+    const {patient, patientName, treatment, date, slot} = booking;
+  
+    var email = {
+      from: process.env.EMAIL_SENDER,
+      to: patient,
+      subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+      text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+      html: `
+        <div>
+          <p> Hello ${patientName}, </p>
+          <h3>Your Appointment for ${treatment} is confirmed</h3>
+          <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+          
+          <h3>Our Address</h3>
+          <p>Andor Killa Bandorban</p>
+          <p>Bangladesh</p>
+          <a href="https://glittery-melomakarona-a711ce.netlify.app/">unsubscribe</a>
+        </div>
+      `
+    };
+  
+    emailClient.sendMail(email, function(err, info){
+      if (err ){
+        console.log(err);
+      }
+      else {
+        console.log('Message sent: ', info);
+      }
+  });
+  
+}
+
+// sender email ends here ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+
 
 async function run() {
     try {
@@ -54,11 +104,24 @@ async function run() {
 
 
 
+        // verifyAdmin middleware token or checking admin or not , if admin then goto to next
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            } else {
+                res.status(403).send({ message: 'forbidden' });
+
+            }
+
+        }
+
 
 
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = servicesCollection.find(query).project({name: 1});
+            const cursor = servicesCollection.find(query).project({ name: 1 });
             const services = await cursor.toArray();
             res.send(services);
         });
@@ -75,31 +138,23 @@ async function run() {
 
 
         // if user not an admin then not show the admin all user panel 
-        app.get('/admin/:email', async(req, res) =>{
+        app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
-            const user = await userCollection.findOne({email: email});
+            const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
-            res.send({admin: isAdmin})
-          })
+            res.send({ admin: isAdmin })
+        })
 
 
-        //user email making admin API here :-
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        //email updater upsert here
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester });
-            if (requesterAccount.role === 'admin') {
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-                const result = await userCollection.updateOne(filter, updateDoc);
-                res.send(result);
-
-            }
-            else {
-                res.status(403).send({ message: 'forbidden' });
-            }
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
 
         })
 
@@ -200,15 +255,40 @@ async function run() {
                 return res.send({ success: false, booking: exists })
             }
             const result = await bookingCollection.insertOne(booking);
+            console.log('email send');
+            sendAppointmentEmail(booking);
+
             return res.send({ success: true, result });
         })
 
 
 
+
+
+
+        // get doctor data for manage section api: 
+        app.get('/doctor',verifyJWT, verifyAdmin, async(req , res) => {
+            const doctors = await doctorCollection.find().toArray();
+            res.send(doctors);
+
+        })
+
         //doctors data api here!
-        app.post('/doctor',async (req , res)=>{
+        app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
             const doctor = req.body;
             const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        })
+
+
+
+
+
+        //doctors delete api here!
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = {email: email};
+            const result = await doctorCollection.deleteOne(filter);
             res.send(result);
         })
 
