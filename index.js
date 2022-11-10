0ed38d021c6455eaeb58e9091d0301c879c1cd3f
Ahsan Habib <ahsanpub16b@gmail.com>
2022-11-10 12:59:57 +0600

    Secure with JWT and Make admin role section done here!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,14 +17,43 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nfgmjj6.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 // console.log(client);
+
+
+// Verify email or user function with jwt:-
+
+function verifyJWT(req, res, next) {
+    // console.log('abc');
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+
+    });
+}
+
+
+
+
+
 async function run() {
     try {
         await client.connect();
         // console.log("Database Connected");
         const servicesCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
+        const userCollection = client.db('doctors_portal').collection('users');
+
+
+
+
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -32,6 +62,63 @@ async function run() {
             res.send(services);
         });
 
+
+
+
+        //all users get method and show them on dashboard All Users:
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+
+
+        // if user not an admin then not show the admin all user panel 
+        app.get('/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+          })
+
+
+        //user email making admin API here :-
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        })
+
+
+
+
+
+        //if email exist then not a create one email one more or if email not exist then create an email here. from useToken and signUp pages.
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
+        })
 
 
         // Warning: This is not the proper way to query multiple collection. 
@@ -70,7 +157,8 @@ async function run() {
          * app.get('/booking:id')//get a specific booking.
          * app.post('/booking:id') // add a new booking.
          * app.patch('/booking:id') // updating a booking.
-         * app.patch('/booking:id') // Deleting a booking.
+         * app.put('/booking:id') // upsert ==> update(if exist)or insert(if doesn't exist)
+         * app.delete('/booking:id') // Deleting a booking.
          * 
          * Moral of the Story :
          * when we write just 'booking' which means all booking collection here . 
@@ -80,12 +168,26 @@ async function run() {
 
 
         // get the email of a single patient 
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
-            const query = { patient: patient };
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                const authorization = req.headers.authorization;
+                // console.log('auth header',authorization);
+                const query = { patient: patient };
+                const bookings = await bookingCollection.find(query).toArray();
+                res.send(bookings);
+
+            } else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+
         })
+
+
+
+
+
 
 
 
